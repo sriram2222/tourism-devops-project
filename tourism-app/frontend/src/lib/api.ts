84149365a572530fd15d8154aa2fa7cdd1c618d4
sriram2222 <raamlakshmanan22@gmail.com>
2026-03-29 00:48:34 +0530

    Fix API URL remove hardcoded port 5000

--- tourism-app/frontend/src/lib/api.ts
+++ tourism-app/frontend/src/lib/api.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const API_BASE =
   typeof window !== "undefined"
-    ? `${window.location.protocol}//${window.location.hostname}:5000/api`
-    : "http://3.110.88.149:5000/api";
+    ? `${window.location.protocol}//${window.location.hostname}/api`
+    : "http://3.110.88.149/api";
 
 const api = axios.create({ baseURL: API_BASE });
 
